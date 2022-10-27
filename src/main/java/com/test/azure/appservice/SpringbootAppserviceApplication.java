7a596731fe145b9019918cc4116321acfc12e65a
bmiriyala <65658893+bmiriyala@users.noreply.github.com>
2022-10-27 13:19:33 +0530

    Add files via upload
    Initial commit src folder and pom.xml

---
+++ src/main/java/com/test/azure/appservice/SpringbootAppserviceApplication.java
@@ -0,0 +1,13 @@
+package com.test.azure.appservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/azure/appservice/SpringbootAppserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.azure.appservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
